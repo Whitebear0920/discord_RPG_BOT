b6491c4f2899a3d0e854fe75784c0cb0f06bb7c4
Whitebear0920 <whiteb0920@gmail.com>
2024-04-29 10:24:52 +0800

    1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, Intents, IntentsBitField } = require('discord.js');
-const { token } = require('token.json')
+const { token } = require('./token.json');
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
@@ -12,3 +12,5 @@ const client = new Client({
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
+
+client.login(token);
