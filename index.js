5eed6d60980c964ac58e3971f6d625960d423595
Whitebear0920 <whiteb0920@gmail.com>
2024-04-29 10:04:06 +0800

    update

---
+++ index.js
@@ -0,0 +1,13 @@
+const { Client, Intents, IntentsBitField } = require('discord.js');
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ]
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
