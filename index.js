8f6e518624b7af3f4023516b9ed21761b8705f4e
Whitebear0920 <whiteb0920@gmail.com>
2024-04-29 14:51:09 +0800

    slash commands

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const { Client, Intents, IntentsBitField } = require('discord.js');
-const { token } = require('./token.json');
+const { Client, IntentsBitField } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
+const { token, clientId, guildId } = require('./config.json');
+const { REST, Routes } = require('discord.js');
+const rest = new REST().setToken(token);
+const commands = require('./commands.json');
+
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
@@ -11,6 +32,22 @@ const client = new Client({
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.guilds.cache.forEach(guild => {
+        console.log(guild.id);
+    });
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+    if (commandName === 'hi') {
+        await interaction.reply('hello');
+    }
 });
 
 client.on('messageCreate', (msg) => {
